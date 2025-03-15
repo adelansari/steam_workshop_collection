620d22445e56a8d9f945d1f9e92ae59d5ca70a36
Adel Ansari <adelansari.a@gmail.com>
2025-03-15 16:36:31 +0200

    Add Microsoft Edge WebDriver configuration

--- steam_collection_bot.py
+++ steam_collection_bot.py
@@ -15,3 +15,21 @@ EDGE_DRIVER_PATH = r"C:\EdgeDriver\msedgedriver.exe"
 
 os.environ['WDM_LOCAL'] = '1'
 os.environ['WDM_SSL_VERIFY'] = '0'
+
+def configure_edge():
+    options = Options()
+    options.add_argument(f"user-data-dir={EDGE_PROFILE_PATH}")
+    options.add_argument("profile-directory=Default")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--no-first-run")
+    options.add_argument("--disable-restore-session-state")
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--allow-running-insecure-content')
+    options.add_argument('--disable-web-security')
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    # Security bypass (temporary)
+    options.set_capability('acceptInsecureCerts', True)
+    
+    service = Service(EDGE_DRIVER_PATH)
+    driver = webdriver.Edge(service=service, options=options)
+    return driver
